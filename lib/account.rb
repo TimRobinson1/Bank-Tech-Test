124da267f406985a2fa9e5d06173d545ce1b434b
Timothy Robinson <timmyrobby@gmail.com>
2017-06-12 11:20:34 +0100

    Balance now displays with two decimal place format

--- lib/account.rb
+++ lib/account.rb
@@ -1,12 +1,13 @@
 # Manages a user's bank account
 class Account
-  attr_reader :name, :balance
+  attr_reader :name
 
   def initialize(name = 'User', starting_balance = 0)
     @name = name
     @balance = starting_balance
   end
 
-  def show_balance
+  def current_balance
+    format('%.2f', @balance)
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,19 +3,24 @@ require 'account'
 describe Account do
   let(:account) { described_class.new('Jane') }
   let(:large_account) { described_class.new('Rich', 3000) }
+  let(:student_account) { described_class.new('Student', 10.54) }
 
   it { is_expected.to be_a Account }
-  it { is_expected.to respond_to :show_balance }
+  it { is_expected.to respond_to :current_balance }
 
   it "holds the account owner's name" do
     expect(account.name).to eq 'Jane'
   end
 
   it 'starts with a default balance of zero' do
-    expect(account.balance).to eq 0
+    expect(account.current_balance).to eq '0.00'
   end
 
   it 'can be set with a higher starting balance' do
-    expect(large_account.balance).to eq 3000
+    expect(large_account.current_balance).to eq '3000.00'
+  end
+
+  it 'can display decimal point values in a balance' do
+    expect(student_account.current_balance).to eq '10.54'
   end
 end
