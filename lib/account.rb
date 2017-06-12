562b6b81a7ec3986f727960ca8b1db33b1b28053
Timothy Robinson <timmyrobby@gmail.com>
2017-06-12 11:10:49 +0100

    New accounts now initialize with a name and balance

--- lib/account.rb
+++ lib/account.rb
@@ -1,6 +1,10 @@
 # Manages a user's bank account
 class Account
-  def initialize
+  attr_reader :name, :balance
+
+  def initialize(name = 'User', starting_balance = 0)
+    @name = name
+    @balance = starting_balance
   end
 
   def show_balance
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,7 +1,21 @@
 require 'account'
 
 describe Account do
-  it { is_expected.to be_a Account }
+  let(:account) { described_class.new('Jane') }
+  let(:large_account) { described_class.new('Rich', 3000) }
 
+  it { is_expected.to be_a Account }
   it { is_expected.to respond_to :show_balance }
+
+  it "holds the account owner's name" do
+    expect(account.name).to eq 'Jane'
+  end
+
+  it 'starts with a default balance of zero' do
+    expect(account.balance).to eq 0
+  end
+
+  it 'can be set with a higher starting balance' do
+    expect(large_account.balance).to eq 3000
+  end
 end
